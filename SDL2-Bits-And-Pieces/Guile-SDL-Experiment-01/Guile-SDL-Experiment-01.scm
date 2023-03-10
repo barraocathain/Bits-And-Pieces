97f5c9b02af0b1f7b827415fc81e69bada8d6feb
Barry Kane <barra@ocathain.ie>
2023-03-10 23:59:55 +0000

    Added infinite-loop.scm

--- SDL2-Bits-And-Pieces/Guile-SDL-Experiment-01/Guile-SDL-Experiment-01.scm
+++ SDL2-Bits-And-Pieces/Guile-SDL-Experiment-01/Guile-SDL-Experiment-01.scm
@@ -8,15 +8,17 @@
 			 (sdl2 events)
 			 (sdl2 input keyboard))
 
-(define (draw ren)
-  (set-renderer-draw-color! ren 255 255 255 255)
+(define (draw renderer)
+  (set-renderer-draw-color! renderer 255 255 255 255)
+
   (let* ((surface (load-image "guile.png"))
-         (texture (surface->texture ren surface)))
+         (texture (surface->texture renderer surface)))
+	
 	(do ((event (poll-event) (poll-event)))
 		((eq? '#t (quit-event? event)))
-	  (clear-renderer ren)
-	  (render-copy ren texture)
-	  (present-renderer ren))))
+	  (clear-renderer renderer)
+	  (render-copy renderer texture)
+	  (present-renderer renderer))))
 
 (sdl-init)
 
